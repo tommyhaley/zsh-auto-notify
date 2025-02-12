945600c9c59dfa4ac9b9de23e087f295e8a6ba07
Tommy Andersson <tommy@haleytek.com>
2025-02-12 22:38:53 +0100

    Add transient toggle

--- auto-notify.plugin.zsh
+++ auto-notify.plugin.zsh
@@ -6,6 +6,9 @@ export AUTO_NOTIFY_VERSION="0.10.2"
 # Threshold in seconds for when to automatically show a notification
 [[ -z "$AUTO_NOTIFY_THRESHOLD" ]] &&
     export AUTO_NOTIFY_THRESHOLD=10
+# Toggle transient notifications
+[[ -z "$AUTO_NOTIFY_ENABLE_TRANSIENT" ]] &&
+    export AUTO_NOTIFY_ENABLE_TRANSIENT=1
 
 # List of commands/programs to ignore sending notifications for
 [[ -z "$AUTO_NOTIFY_IGNORE" ]] &&
@@ -52,7 +55,7 @@ function _auto_notify_message() {
 
     if [[ "$platform" == "Linux" ]]; then
         local urgency="normal"
-        local transient="--hint=int:transient:1"
+        local transient="--hint=int:transient:$AUTO_NOTIFY_ENABLE_TRANSIENT"
         local icon=${AUTO_NOTIFY_ICON_SUCCESS:-""}
         # Exit code 130 is returned when a process is terminated with SIGINT.
         # Since the user is already interacting with the program, there is no
